97c841ab0eb6952e9dd3ee8a078b22495568a564
Salesforce-Developer9 <family94173@gmail.com>
2024-06-02 15:51:20 +0530

    missing components

---
+++ .sfdx/typings/lwc/apex/FlexTemplateController.d.ts
@@ -0,0 +1,3 @@
+declare module "@salesforce/apex/FlexTemplateController.getFlexTemplateResponse" {
+  export default function getFlexTemplateResponse(param: {AccName: any, rating: any}): Promise<any>;
+}
